62ff67d9d958e1b31887ca73e67bb1a4cf5bd533
Tariful Hasan <tarif305@gmail.com>
2026-01-14 22:15:01 +0600

    project setup

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layouts/RootLayout";
+import Home from "../Components/Home";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout/>,
+    children: [{
+        path: "/",
+        element: <Home/>
+    }]
+  },
+]);
